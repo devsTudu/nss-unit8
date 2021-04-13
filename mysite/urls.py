94866792f5a3d950572292d98eba301c12e85ae5
Debasish T <>
2021-04-13 14:32:38 +0000

    for heroku

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from home.views import homepage, post_detail, showgallery, seeMembers
 
